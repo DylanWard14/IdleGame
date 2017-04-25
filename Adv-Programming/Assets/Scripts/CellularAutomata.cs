038e79b6552e1e448955646f72ff8bd2e2004001
Dylan Ward <dylan.ward@iinet.net.au>
2017-04-25 10:35:38 +1000

    Cellular Automata Complete

--- Adv-Programming/Assets/Scripts/CellularAutomata.cs
+++ Adv-Programming/Assets/Scripts/CellularAutomata.cs
@@ -18,6 +18,11 @@ public class CellularAutomata : MonoBehaviour
 
         GenerateGrid();
 
+        for (int i = 0; i < 4; i++)
+        {
+            SmoothGrid();
+        }
+
         SpawnObjects();
     }
 	
@@ -35,7 +40,7 @@ public class CellularAutomata : MonoBehaviour
             {
                 Debug.Log("Running");
                 int randNumber = Random.Range(0, 100);
-                if (randNumber <= probability)
+                if (randNumber <= probability && x != 0 && x != grid.GetLength(0) - 1 && y != 0 && y != grid.GetLength(1) - 1)
                 {
                     grid[x, y] = 0;
                 }
@@ -45,6 +50,45 @@ public class CellularAutomata : MonoBehaviour
                 }
             }
         }
+    }
+
+    void SmoothGrid()
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                if (x != 0 && x != grid.GetLength(0) -1 && y != 0 && y != grid.GetLength(1) -1)
+                {
+                    if (GetAmountOfNeighbours(x, y) > 4)
+                    {
+                        grid[x, y] = 1;
+                    }
+                    else if (GetAmountOfNeighbours(x, y) < 4)
+                    {
+                        grid[x, y] = 0;
+                    } 
+                }
+            }
+        }
+    }
+
+    int GetAmountOfNeighbours(int gridX, int gridY)
+    {
+        int neighbours = 0;
+
+        for (int x = gridX - 1; x <= gridX + 1; x++)
+        {
+            for (int y = gridY - 1; y <= gridY + 1; y++)
+            {
+                if (grid[x, y] == 1)
+                {
+                    neighbours++;
+                }
+            }
+        }
+
+        return neighbours;
     }
 
     void SpawnObjects()
