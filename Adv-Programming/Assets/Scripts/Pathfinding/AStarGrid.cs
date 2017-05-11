c0b7802e1306117ff91d55b199fd7de8d81b51db
Dylan Ward <dylan.ward@iinet.net.au>
2017-05-11 17:37:56 +1000

    Buggy A* incorrectly calculating neighbours.

--- Adv-Programming/Assets/Scripts/Pathfinding/AStarGrid.cs
+++ Adv-Programming/Assets/Scripts/Pathfinding/AStarGrid.cs
@@ -11,7 +11,7 @@ public class AStarGrid : MonoBehaviour
     public AStarNode[,] grid;
     public int gridWorldSizeX;
     public int gridWorldSizeY;
-    public LayerMask walkableMask;
+    public LayerMask unwalkableMask;
     public CellularAutomata cellularAutomata;
 
     public GameObject cube;
@@ -22,10 +22,10 @@ public class AStarGrid : MonoBehaviour
         nodeDiameter = nodeRadius * 2;
         gridSizeX = (int)(gridWorldSizeX / nodeDiameter);
         gridSizeY = (int)(gridWorldSizeY / nodeDiameter);
-        CreateGrid(grid);
+        CreateGrid();
     }
 
-    void CreateGrid(AStarNode[,] grid)
+    void CreateGrid()
     {
         Vector3 worldBottomLeft = new Vector3(-gridSizeX / 2 * nodeDiameter, 0, -gridSizeY / 2 * nodeDiameter);
         Debug.Log(worldBottomLeft);
@@ -36,12 +36,13 @@ public class AStarGrid : MonoBehaviour
             for (int y = 0; y < grid.GetLength(1); y++)
             {
                 Vector3 worldPosition = new Vector3(worldBottomLeft.x + x, 0, worldBottomLeft.z + y);
-                bool walkable = false;
 
+                /*bool walkable = false;
                 if (cellularAutomata.grid[x, y] == 0)
                 {
                     walkable = true;
-                }
+                }*/
+                bool walkable = !(Physics.CheckSphere(worldPosition, nodeRadius, unwalkableMask));
 
                 grid[x, y] = new AStarNode(walkable, worldPosition, x, y);
             }
@@ -51,9 +52,10 @@ public class AStarGrid : MonoBehaviour
     public AStarNode WorldToGridPos(Vector3 worldPos)
     {
         float gridPercentX = (worldPos.x + gridWorldSizeX / 2) / gridWorldSizeX;
-        float gridPercentY = (worldPos.y + gridWorldSizeY / 2) / gridWorldSizeY;
+        float gridPercentY = (worldPos.z + gridWorldSizeY / 2) / gridWorldSizeY;
 
         gridPercentX = Mathf.Clamp01(gridPercentX);
+        gridPercentY = Mathf.Clamp01(gridPercentY);
 
         int xPosition = Mathf.RoundToInt((gridSizeX - 1) * gridPercentX);
         int yPosition = Mathf.RoundToInt((gridSizeY - 1) * gridPercentY);
@@ -63,9 +65,9 @@ public class AStarGrid : MonoBehaviour
 
     public List<AStarNode> FindNeighbours(AStarNode node)
     {
-        List<AStarNode> neighbours = new List<AStarNode>;
+        List<AStarNode> neighbours = new List<AStarNode>();
 
-        for (int x = node.gridPositionX - 1; x < node.gridPositionX + 1; x++)
+        for (int x = node.gridPositionX - 1; x < node.gridPositionX + 1; x++) // this doesnt work, adds the same node each time.
         {
             for (int y = node.gridPositionY - 1; y < node.gridPositionY + 1; y++)
             {
@@ -78,7 +80,58 @@ public class AStarGrid : MonoBehaviour
             }
         }
 
+        /*for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+
+                int checkX = node.gridPositionX + x;
+                int checkY = node.gridPositionY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }*/
+
         return neighbours;
+    }
+
+    public List<AStarNode> path;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSizeX, 1, gridWorldSizeY));
+
+        if (grid != null)
+        {
+            Debug.Log("Grid Not null");
+            foreach (AStarNode n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if (path != null)
+                {
+                   // Debug.Log("Path Not null");
+                    if (path.Contains(n))
+                    {
+                        Gizmos.color = Color.black;
+                    }
+                }
+                else
+                {
+                    //Debug.Log("Path null");
+                }
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+        }
+
+        else
+        {
+            Debug.Log("Grid Null");
+        }
     }
 
 }
---
+++ Adv-Programming/Assets/Scripts/Pathfinding/Pathing.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathing : MonoBehaviour
+{
+    AStarGrid grid;
+
+    public Transform seeker, target;
+
+    private void Awake()
+    {
+        grid = GetComponent<AStarGrid>();
+    }
+
+    private void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+
+    void FindPath(Vector3 startPos, Vector3 endPos)
+    {
+        AStarNode startNode = grid.WorldToGridPos(startPos);
+        AStarNode endNode = grid.WorldToGridPos(endPos);
+
+        List<AStarNode> openSet = new List<AStarNode>();
+        HashSet<AStarNode> closedSet = new HashSet<AStarNode>();
+
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)//
+        {
+            AStarNode currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++) //
+            {
+                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost) 
+                {
+                    if (openSet[i].hCost < currentNode.hCost)//
+                    {
+                        currentNode = openSet[i]; 
+                    }
+                }
+            }
+
+            openSet.Remove(currentNode);
+
+            closedSet.Add(currentNode);
+
+            if (currentNode == endNode)
+            {
+                //retrace path
+                RetracePath(startNode, endNode);
+                return;
+            }
+
+            foreach (AStarNode neighbour in grid.FindNeighbours(currentNode))
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+
+                int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+
+                if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newMovementCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, endNode);
+                    neighbour.parentNode = currentNode;
+
+                    if (!openSet.Contains(neighbour))
+                    {
+                        openSet.Add(neighbour);
+                    }
+                }
+            }
+        }
+    }
+
+    void RetracePath(AStarNode start, AStarNode end)
+    {
+        List<AStarNode> path = new List<AStarNode>();
+        AStarNode currentNode = end;
+
+        while (currentNode != start)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parentNode;
+        }
+
+        path.Reverse();
+        grid.path = path;
+    }
+
+    public int GetDistance(AStarNode nodeA, AStarNode nodeB)
+    {
+        int distanceX = Mathf.Abs(nodeA.gridPositionX - nodeB.gridPositionX);
+        int distanceY = Mathf.Abs(nodeA.gridPositionY - nodeB.gridPositionY);
+
+        if (distanceX > distanceY)
+        {
+            return 14 * distanceY + 10 * (distanceX - distanceY);
+        }
+        else
+        {
+            return 14 * distanceX + 10 * (distanceY - distanceX);
+        }
+    }
+}
