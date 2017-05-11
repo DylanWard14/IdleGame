9cfa43270c28ed112584175a5b6d91400d6f2505
Dylan Ward <dylan.ward@iinet.net.au>
2017-05-11 15:19:25 +1000

    Completion of the A* Grid

--- Adv-Programming/Assets/Scripts/Pathfinding/AStarGrid.cs
+++ Adv-Programming/Assets/Scripts/Pathfinding/AStarGrid.cs
@@ -35,18 +35,50 @@ public class AStarGrid : MonoBehaviour
         {
             for (int y = 0; y < grid.GetLength(1); y++)
             {
-                Vector3 worldPosition = new Vector3(worldBottomLeft.x + x, 0, worldBottomLeft.y + y);
+                Vector3 worldPosition = new Vector3(worldBottomLeft.x + x, 0, worldBottomLeft.z + y);
                 bool walkable = false;
 
                 if (cellularAutomata.grid[x, y] == 0)
                 {
                     walkable = true;
-                    Instantiate(cube, worldPosition, Quaternion.identity);
                 }
 
                 grid[x, y] = new AStarNode(walkable, worldPosition, x, y);
             }
         }
+    }
+
+    public AStarNode WorldToGridPos(Vector3 worldPos)
+    {
+        float gridPercentX = (worldPos.x + gridWorldSizeX / 2) / gridWorldSizeX;
+        float gridPercentY = (worldPos.y + gridWorldSizeY / 2) / gridWorldSizeY;
+
+        gridPercentX = Mathf.Clamp01(gridPercentX);
+
+        int xPosition = Mathf.RoundToInt((gridSizeX - 1) * gridPercentX);
+        int yPosition = Mathf.RoundToInt((gridSizeY - 1) * gridPercentY);
+
+        return grid[xPosition, yPosition];
+    }
+
+    public List<AStarNode> FindNeighbours(AStarNode node)
+    {
+        List<AStarNode> neighbours = new List<AStarNode>;
+
+        for (int x = node.gridPositionX - 1; x < node.gridPositionX + 1; x++)
+        {
+            for (int y = node.gridPositionY - 1; y < node.gridPositionY + 1; y++)
+            {
+                if (x == node.gridPositionX && y == node.gridPositionY)
+                    continue;
+                else if (x >= 0 || x <= gridSizeX || y >= 0 || y <= gridSizeY)
+                {
+                    neighbours.Add(node);
+                }
+            }
+        }
+
+        return neighbours;
     }
 
 }
